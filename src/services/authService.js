e57306646808bcd976d63e55f5ad7d6952836ade
Viniccius Wirz <vinimovel23@gmail.com>
2023-05-26 11:55:26 -0300

    Fix: import filename

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,7 @@ import {
   checkUserEmailDB,
   checkUsernameDB,
 } from "../repositories/auth.repository.js";
-import genDefUsername from "../utils/GenDefUsername.js";
+import genDefUsername from "../utils/genDefUsername.js";
 import bcrypt from "bcrypt";
 import { getProfilePicDB } from "../repositories/images.repository.js";
 
@@ -23,15 +23,20 @@ export async function createNewUsername() {
 export async function authenticateUser(email, password) {
   try {
     const { rows, rowCount } = await checkUserEmailDB(email);
+
     if (!rowCount) {
       return { error: { status: 404 } };
     }
+
     const userPassword = rows[0].password;
+
     const comparePassword = bcrypt.compareSync(password, userPassword);
     if (!comparePassword) return { error: { status: 401 } };
+
     const userId = rows[0].id;
     const { rows: image } = await getProfilePicDB(userId);
     const pfp = image[0].image_url;
+
     return {
       id: userId,
       name: rows[0].name,
